23f95b4c6fa03eadcd879cff3fccf3b0c2e0eea3
tduncan14 <nbatre1@yahoo.com>
2026-06-06 15:09:42 -0500

    Initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+
+dotenv.config
+
+
+const app = express();
+const PORT = process.env.PORT || 5000
+
+
+
+app.listen(5000, () => {
+
+    console.log("Server is running on PORT 5000")
+
+
+})
